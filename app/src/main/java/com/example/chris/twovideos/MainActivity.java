2fe1294dee61ea4761d2b50f2a426cdc5b803fb9
Christian Kleineidam <christian.kleineidam@gmail.com>
2018-05-31 17:07:47 +0200

    Illustrate the problem of black screen flashing between videos

--- app/src/main/java/com/example/chris/twovideos/MainActivity.java
+++ app/src/main/java/com/example/chris/twovideos/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.chris.twovideos;
 
 
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.RawResourceDataSource;
+import com.google.android.exoplayer2.util.Util;
+
 public class MainActivity extends AppCompatActivity {
+    public SimpleExoPlayer getPlayer() {
+        return mPlayer;
+    }
+
+    public SimpleExoPlayer mPlayer;
+    private DataSource.Factory mDataSourceFactory;
+    private boolean videoChoice;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +43,65 @@ public class MainActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createNewFragment();
+                BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+                TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+                TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+                mPlayer = ExoPlayerFactory.newSimpleInstance(MainActivity.this, trackSelector);
+                if (videoChoice){
+                    playVideo(MainActivity.this, "collect");
+                }
+                else{
+                    playVideo(MainActivity.this, "chair");
+                }
+                videoChoice = !videoChoice;
+                waitTillReady();
             }
         });
     }
+
+    private void waitTillReady(){
+        if (mPlayer.getPlaybackState() == Player.STATE_READY){
+            createNewFragment();
+        }
+        else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    waitTillReady();
+                }
+            }, 10);
+        }
+    }
+
+
 
 
     private void createNewFragment() {
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.contentFragment, new VideoFragment());
         ft.commit();
+    }
+
+    private DataSource.Factory getDataSourceFactory(){
+        if (mDataSourceFactory==null){
+            mDataSourceFactory = new DefaultDataSourceFactory(
+                    this,
+                    Util.getUserAgent(this, this.getPackageName()) ) ;
+        }
+
+        return mDataSourceFactory;
+    }
+
+    private void playVideo(Context context, String name){
+        final int raw_res_id = context.getResources().getIdentifier(
+                name,
+                "raw",
+                context.getPackageName());
+        MediaSource media_source = new ExtractorMediaSource.Factory(getDataSourceFactory()).createMediaSource(
+                RawResourceDataSource.buildRawResourceUri(raw_res_id));
+        mPlayer.prepare(media_source);
+        mPlayer.setPlayWhenReady(false);
+
     }
 }
--- app/src/main/java/com/example/chris/twovideos/VideoFragment.java
+++ app/src/main/java/com/example/chris/twovideos/VideoFragment.java
@@ -28,38 +28,20 @@ import com.google.android.exoplayer2.util.Util;
 public class VideoFragment extends Fragment{
     private Context mContext;
     private SimpleExoPlayer mPlayer;
-    private SurfaceView mSurfaceView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.videolayout, container, false);
         mContext = inflater.getContext();
+        MainActivity activity = (MainActivity) getActivity();
+        mPlayer = activity.getPlayer();
 
-        mSurfaceView = view.findViewById(R.id.surface_view1);
-
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-
-        mPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector);
-        mPlayer.setVideoSurfaceView(mSurfaceView);
+        mPlayer.setVideoSurfaceView((SurfaceView) view.findViewById(R.id.surface_view1));
         mPlayer.setPlayWhenReady(true);
-        playVideo(mContext);
 
         return view;
     }
 
-    private void playVideo(Context context){
-        int raw_res_id = context.getResources().getIdentifier(
-                "collect",
-                "raw",
-                context.getPackageName());
-        DataSource.Factory factory = new DefaultDataSourceFactory(
-                mContext,
-                Util.getUserAgent(mContext, context.getPackageName()) ) ;
-        MediaSource media_source = new ExtractorMediaSource.Factory(factory).createMediaSource(
-                RawResourceDataSource.buildRawResourceUri(raw_res_id));
-        mPlayer.prepare(media_source);
-    }
+
 }
